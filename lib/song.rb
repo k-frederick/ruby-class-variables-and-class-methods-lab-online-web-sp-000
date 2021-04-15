4be6bb49db1be0a5a1c1c6d3a36a9a212cf5e477
Kristina Frederick <kristinafrederick@icloud.com>
2021-04-15 22:58:04 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song
+  @@count = 0
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def artist=(artist)
+    @artist = artist
+  end
+  def artist
+    @artist
+  end
+  def genre=(genre)
+    @genre = genre
+  end
+  def genre
+    @genre
+  end
+  def self.count
+    @@count
+  end
+end
